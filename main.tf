f790ebc1fde775441437b01cb6a6668815aef503
hpal <ahosea15@gmail.com>
2023-12-22 11:16:28 +0000

    remove resource group for now

--- main.tf
+++ main.tf
@@ -19,11 +19,6 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = "westus2"
-}
-
 resource "azurerm_storage_account" "storage_account" {
   name                = var.storage_account_name
   resource_group_name = var.resource_group_name
