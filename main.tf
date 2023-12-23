57f06853b057a3618595682488f76177c2af0572
hpal <ahosea15@gmail.com>
2023-12-23 18:20:52 +0000

    make changes to terraform config

--- main.tf
+++ main.tf
@@ -5,18 +5,23 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0.2"
     }
+    port = {
+      source  = "port-labs/port-labs"
+      version = "~> 1.0.0"
+    }
   }
 
   required_version = ">= 1.1.0"
 }
 
 provider "azurerm" {
-  client_id       =   var.client_id
-  client_secret   =   var.client_secret
-  subscription_id =   var.subscription_id
-  tenant_id       =   var.tenant_id
-  
+
   features {}
+}
+
+provider "port" {
+  client_id = var.port_client_id     # or set the env var PORT_CLIENT_ID
+  secret    = var.port_client_secret # or set the env var PORT_CLIENT_SECRET
 }
 
 resource "azurerm_storage_account" "storage_account" {
@@ -27,4 +32,20 @@ resource "azurerm_storage_account" "storage_account" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
   account_kind             = "StorageV2"
+}
+
+resource "port_entity" "azure_storage_account" {
+  count      = azurerm_storage_account.storage_account.primary_web_endpoint ? 1 : 0
+  identifier = var.storage_account_name
+  title      = var.storage_account_name
+  blueprint  = "azureStorage"
+  run_id     = var.port_run_id
+  properties = {
+    string_props = {
+      "storage_name"     = var.storage_account_name,
+      "storage_location" = var.location
+    }
+  }
+
+  depends_on = [azurerm_storage_account.storage_account]
 }
--- variables.tf
+++ variables.tf
@@ -1,29 +1,5 @@
 # Service Principal Variables
 
-variable "client_id" {
-    description =   "Client ID (APP ID) of the application"
-    default     = "XXXXXX-1111-2222-3333-YYYYYYYYYY"
-    type        =   string
-}
-
-variable "client_secret" {
-    description =   "Client Secret (Password) of the application"
-    default     = "XXXXXX-1111-2222-3333-YYYYYYYYYY"
-    type        =   string
-}
-
-variable "subscription_id" {
-    description =   "Subscription ID"
-    default     = "XXXXXX-1111-2222-3333-YYYYYYYYYY"
-    type        =   string
-}
-
-variable "tenant_id" {
-    description =   "Tenant ID"
-    default     = "XXXXXX-1111-2222-3333-YYYYYYYYYY"
-    type        =   string
-}
-
 variable "resource_group_name" {
   type        = string
   default     = "myTFResourceGroup"
@@ -40,4 +16,19 @@ variable "storage_account_name" {
   type        = string
   description = "Storage Account name in Azure"
   default     = "demo"
+}
+
+variable "port_run_id" {
+  type        = string
+  description = "The runID of the action run that created the entity"
+}
+
+variable "port_client_id" {
+  type        = string
+  description = "The runID of the action run that created the entity"
+}
+
+variable "port_client_secret" {
+  type        = string
+  description = "The runID of the action run that created the entity"
 }
