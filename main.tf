c0edc0ffee808ea5777a1ac16c3583b541cd5dda
hpal <ahosea15@gmail.com>
2024-01-09 14:48:19 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,12 +15,6 @@ terraform {
 }
 
 provider "azurerm" {
-  
-  subscription_id = var.az_subscription_id
-
-  client_id       = var.az_client_id
-  client_secret   = var.az_client_secret
-  tenant_id       = var.az_tenant_id
 
   features {}
 }
