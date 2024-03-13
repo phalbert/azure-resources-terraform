5cd8f7915ff6c5d7540c61b658c2982e253c7a0b
hpal <ahosea15@gmail.com>
2024-03-13 17:46:23 +0000

    add sending api

--- scripts/send_slack_notifications.py
+++ scripts/send_slack_notifications.py
@@ -1,10 +1,42 @@
-import json
-import sys
+import logging
+import os
 import requests
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class PortClient:
+    def __init__(self, client_id: str, client_secret: str):
+        self.api_url = "https://api.getport.io"
+        self.access_token = self.get_token(client_id, client_secret)
+        self.headers = {
+            "Authorization": f"Bearer {self.access_token}",
+            "User-Agent": "port-message-service",
+        }
+
+    def get_token(self, client_id, client_secret):
+        credentials = {"clientId": client_id, "clientSecret": client_secret}
+        token_response = requests.post(
+            f"{self.api_url}/v1/auth/access_token", json=credentials
+        )
+        token_response.raise_for_status()
+        return token_response.json()["accessToken"]
+
+    def search_entities(self, query):
+        search_req = requests.post(
+            f"{self.api_url}/v1/entities/search",
+            json=query,
+            headers=self.headers,
+            params={},
+        )
+        search_req.raise_for_status()
+        return search_req.json()["entities"]
+
+
 def send_notification(entity, message, api):
-    title = entity['title']
-    slack_webhook = entity['properties'].get('slack') 
+    title = entity["title"]
+    slack_webhook = entity["properties"].get("slack")
 
     if slack_webhook:
         payload = {
@@ -14,15 +46,33 @@ def send_notification(entity, message, api):
     else:
         print(f"No Slack webhook found for: {title}")
 
-if __name__ == "__main__":
-    entities_file = sys.argv[1]
-    message = sys.argv[2]
-    api = sys.argv[3]
 
-    print(f"Sending notification to entities in {entities_file} with message: {message}")
+if __name__ == "__main__":
+    port_client_id = os.environ.get("PORT_CLIENT_ID")
+    port_client_secret = os.environ.get("PORT_CLIENT_SECRET")
+    message = os.environ.get("MESSAGE")
+    sending_api = os.environ.get("SENDING_API")
+
+    print("Initializing Port client", port_client_id, port_client_secret)
+    port_client = PortClient(port_client_id, port_client_secret)
     
-    with open(entities_file) as f:
-        entities = json.load(f)
+    logger.info(f"Fetching entities for query: sending_api {sending_api},")
+    search_query = {
+        "combinator": "and",
+        "rules": [
+            {"property": "$blueprint", "operator": "=", "value": "service"},
+            {
+                "blueprint": "api",
+                "operator": "relatedTo",
+                "value": sending_api,
+            },
+        ],
+    }
+
+    entities = port_client.search_entities(search_query)
+
+    print(f"Found {len(entities)} entities for {sending_api}")
 
     for entity in entities:
-        send_notification(entity, message, api)
+        print(f"Sending notification to {entity['title']}")
+        send_notification(entity, message, sending_api)
