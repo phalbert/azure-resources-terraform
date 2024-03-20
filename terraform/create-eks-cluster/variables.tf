7701801fede983021188b41971935ba3a61cf661
hpal <ahosea15@gmail.com>
2024-03-20 23:16:03 +0000

    add config dir

---
+++ terraform/create-eks-cluster/variables.tf
@@ -0,0 +1,36 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-west-1"
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
+
+variable "cluster_version" {
+  description = "Version of the EKS cluster"
+  type        = string
+  default     = "1.27"
+  
+}
+
+variable "port_run_id" {
+  type        = string
+  description = "The runID of the action run that created the entity"
+  default     = ""
+}
+
+variable "port_client_id" {
+  type        = string
+  description = "The Port client ID"
+}
+
+variable "port_client_secret" {
+  type        = string
+  description = "The Port client secret"
+}
