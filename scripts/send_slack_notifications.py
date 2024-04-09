2f7e0de63a7d4d5ac1076305776d86edd594f3ea
hpal <ahosea15@gmail.com>
2024-04-09 17:13:45 +0100

    log more

--- scripts/send_slack_notifications.py
+++ scripts/send_slack_notifications.py
@@ -93,7 +93,6 @@ if __name__ == "__main__":
     for entity in entities:
         if "relations" in entity and "consumes_api" in entity.get("relations"):
             consumes_api = entity["relations"]["consumes_api"]
-            print(f"Consumes APIs: {consumes_api}")
             if sending_api in consumes_api:
                 print(f"Sending notification to {entity['title']}")
                 send_notification(entity, message, sending_api)
